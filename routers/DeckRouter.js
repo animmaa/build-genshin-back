2138b0bcce7e2db71642af10b6b3c72790d19674
davy <davylebris@gmail.com>
2023-05-11 11:17:27 +0200

    controller ok

--- routers/DeckRouter.js
+++ routers/DeckRouter.js
@@ -2,27 +2,16 @@ const deckController = require('../controller/deck-controller');
 
 const deckRouter = require('express').Router();
 
-
 deckRouter.get('/', deckController.getAllDeck);
-
 deckRouter.get('/fulldeck', deckController.getDeckCompletelyFilled);
-
 deckRouter.get('/number/:deck_id/:card_id', deckController.getNumberCardInTheDeck);
-
 deckRouter.get('/:id', deckController.getAllDeckForOneUser);
-
 deckRouter.get('/infosdeck/:id', deckController.getAllInfosForOneDeck);
-
 deckRouter.get('/cardlist/:id', deckController.getAllCardForOneDeck);
-
 deckRouter.get('/totalpersonnage/:id', deckController.getPersonnageNumberCardInTheDeck);
-
 deckRouter.get('/totalcard/:id', deckController.getTotalNumberCardInTheDeck);
-
 deckRouter.post('/deckadd/:id', deckController.postCreateDeck);
-
 deckRouter.put('/updatedeck/:id', deckController.putUpdateImageAndNameDeck);
-
 deckRouter.delete('/deckdelete/:id', deckController.deleteOneDeck);
 
 module.exports = deckRouter;
